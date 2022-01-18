91f0f5ebeaae2407797798b70110e6983b8818d1
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-18 11:48:56 -0500

    Events modifiers

--- app.js
+++ app.js
@@ -2,8 +2,38 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: 'Manuel Escobar',
+      confirmedName: '',
     };
+  },
+  methods: {
+    submitForm() {
+      alert('submitted!');
+    },
+    setName({
+      target: {
+        value, 
+      } 
+    }) {
+      this.name = value;
+    },
+    confirmInput({
+      target: {
+        value, 
+      } 
+    }) {
+      this.confirmedName = value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    reset() {
+      this.counter = 0;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
